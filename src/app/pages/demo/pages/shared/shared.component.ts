342ee617568f0db0aea390cf6579150971b13e09
Wilson Oquendo <oquendo999@hotmail.com>
2023-11-27 08:32:10 -0500

    feat: added input module

--- src/app/pages/demo/pages/shared/shared.component.ts
+++ src/app/pages/demo/pages/shared/shared.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validator} from '@angular/forms';
 
 @Component({
   selector: 'app-shared',
   templateUrl: './shared.component.html',
   styleUrls: ['./shared.component.scss']
 })
-export class SharedComponent {
+export class SharedComponent implements OnInit {
+  form!: FormGroup;
 
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      input: [null]
+    })
+  }
+
+  onPatchValue(){
+    this.form.patchValue({input: 'Ingrese un valor'})
+  }
+
+  onSubmit(): void{
+    console.log('Se presiona submit');
+  }
 }
